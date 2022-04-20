1556fc0802a2b20ed30a6d23abd7c585ef7bb446
Ariel G. Quindoza Jr <quindozajrariel@gmail.com>
2022-04-20 12:57:02 +0800

    Add templating.

--- index.php
+++ index.php
@@ -4,13 +4,31 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '/':
+        require __DIR__ . '/pages/template/header.php';
         require __DIR__ . '/pages/home.php';
+        require __DIR__ . '/pages/template/sub_footer.php';
+        require __DIR__ . '/pages/home_js.php';
+        require __DIR__ . '/pages/template/footer.php';
         break;
     case '':
+        require __DIR__ . '/pages/template/header.php';
         require __DIR__ . '/pages/home.php';
+        require __DIR__ . '/pages/template/sub_footer.php';
+        require __DIR__ . '/pages/home_js.php';
+        require __DIR__ . '/pages/template/footer.php';
         break;
     case '/home':
+        require __DIR__ . '/pages/template/header.php';
         require __DIR__ . '/pages/home.php';
+        require __DIR__ . '/pages/template/sub_footer.php';
+        require __DIR__ . '/pages/home_js.php';
+        require __DIR__ . '/pages/template/footer.php';
+        break;
+    case '/contact-us':
+        require __DIR__ . '/pages/template/header.php';
+        require __DIR__ . '/pages/contact_us.php';
+        require __DIR__ . '/pages/template/sub_footer.php';
+        require __DIR__ . '/pages/template/footer.php';
         break;
     default:
         http_response_code(404);
---
+++ pages/home_js.php
@@ -0,0 +1,30 @@
+<script>
+    $(document).ready(function() {
+        type_1()
+    })
+    var i1 = 0;
+    var i2 = 0;
+
+    var txt1 = 'Focus on building your life,';
+    var txt2 = 'Let us handle your finances';
+
+    var speed = 50;
+
+    function type_1() {
+        if (i1 < txt1.length) {
+            document.getElementById("tagline1").innerHTML += txt1.charAt(i1);
+            i1++;
+            setTimeout(type_1, speed);
+        } else {
+            type_2()
+        }
+    }
+
+    function type_2() {
+        if (i2 < txt2.length) {
+            document.getElementById("tagline2").innerHTML += txt2.charAt(i2);
+            i2++;
+            setTimeout(type_2, speed);
+        }
+    }
+</script>
