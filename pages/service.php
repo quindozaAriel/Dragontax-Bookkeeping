43c9b0c217b743e39ad015bcda6f1b6615a271c3
Ariel G. Quindoza Jr <quindozajrariel@gmail.com>
2022-06-27 20:36:43 +0800

    [SERVICE] - Add landing section.

--- pages/service.php
+++ pages/service.php
@@ -1,5 +1,15 @@
-<div class="container">
-    <div class="row">
-        <div class="col"></div>
+<div class="section__a">
+    <div class="container p-5 px-md-4 relative">
+        <div class="row">
+            <div class="col-12 col-md-7 col-lg-6 col-xl-6">
+                <div class="service_heading">
+                    <h1>Bookkeeping Becomes Easy With Us </h1>
+                    <p class="mt-3 mb-4">We understand that your business is an important part of your life, and we are dedicated to helping you increase your business efficiency and saving you time.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-5 col-lg-6 col-xl-6">
+                <div class="screen_image my-5 my-md-0"></div>
+            </div>
+        </div>
     </div>
 </div>
