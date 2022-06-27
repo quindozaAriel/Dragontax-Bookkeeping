569c847d8d092e869b9e35391d89f0663c7d54ac
Ariel G. Quindoza Jr <quindozajrariel@gmail.com>
2022-06-27 22:27:08 +0800

    [SERVICE] - Add how we work section.

--- pages/service.php
+++ pages/service.php
@@ -13,3 +13,40 @@
         </div>
     </div>
 </div>
+<div class="container py-5">
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+            <h1><b>How We Work</b></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 col-md-4">
+            <div class="works_card px-5 pb-2 p-md-0 px-md-1 px-lg-2 px-xl-3">
+                <img class="work-clipart" src="../assets/build/svgs/ready.svg" alt="What we do clipart">
+                <label>We got you ready</label>
+                <p>
+                    We’ll work with you to understand your business and how Dragon Tax works, then provide a dedicated bookkeeper who’ll get to know your business and gather all the information they need from you.
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-4">
+            <div class="works_card px-5 pb-2 p-md-0 px-md-1 px-lg-2 px-xl-3">
+                <img class="work-clipart" src="../assets/build/svgs/working.svg" alt="What we do clipart">
+                <label>We work on your bookkeeping</label>
+                <p>
+                    Your bookkeeper organize all of your transactions, including invoices and receipts, as well as prepare financial statements that you can use to assess the health of your operations. Will help you understand exactly where any issues lie, so that you can make necessary changes and address any issues immediately.
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-md-4">
+            <div class="works_card px-5 pb-2 p-md-0 px-md-1 px-lg-2 px-xl-3">
+                <img class="work-clipart" src="../assets/build/svgs/deliver.svg" alt="What we do clipart">
+                <label>We deliver your tax-ready financials</label>
+                <p>
+                    We help you make sense of your company’s finances. Track business health with monthly financial reporting, providing insight into what drives business performance. At tax time, we send a package with everything you need to file.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div></div>
