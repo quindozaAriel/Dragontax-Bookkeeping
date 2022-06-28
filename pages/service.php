c1d4ba2f4c0153dc5a984942d2e4cb982c4d946f
Ariel G. Quindoza Jr <quindozajrariel@gmail.com>
2022-06-28 10:55:45 +0800

    [SERVICE] - Add tagline banner.

--- pages/service.php
+++ pages/service.php
@@ -2,13 +2,13 @@
     <div class="container p-5 px-md-4 relative">
         <div class="row">
             <div class="col-12 col-md-7 col-lg-6 col-xl-6">
-                <div class="service_heading">
+                <div class="service_heading" data-aos="fade-up">
                     <h1>Bookkeeping Becomes Easy With Us </h1>
                     <p class="mt-3 mb-4">We understand that your business is an important part of your life, and we are dedicated to helping you increase your business efficiency and saving you time.</p>
                 </div>
             </div>
             <div class="col-12 col-md-5 col-lg-6 col-xl-6">
-                <div class="screen_image my-5 my-md-0"></div>
+                <div class="screen_image my-5 my-md-0" data-aos="fade-left"></div>
             </div>
         </div>
     </div>
@@ -22,7 +22,7 @@
     <div class="row">
         <div class="col-12 col-md-4">
             <div class="works_card px-5 pb-2 p-md-0 px-md-1 px-lg-2 px-xl-3">
-                <img class="work-clipart" src="../assets/build/svgs/ready.svg" alt="What we do clipart">
+                <img class="work-clipart" src="../assets/build/svgs/ready.svg" alt="What we do clipart" data-aos="fade-right">
                 <label>We got you ready</label>
                 <p>
                     We’ll work with you to understand your business and how Dragon Tax works, then provide a dedicated bookkeeper who’ll get to know your business and gather all the information they need from you.
@@ -31,7 +31,7 @@
         </div>
         <div class="col-12 col-md-4">
             <div class="works_card px-5 pb-2 p-md-0 px-md-1 px-lg-2 px-xl-3">
-                <img class="work-clipart" src="../assets/build/svgs/working.svg" alt="What we do clipart">
+                <img class="work-clipart" src="../assets/build/svgs/working.svg" alt="What we do clipart" data-aos="fade-up">
                 <label>We work on your bookkeeping</label>
                 <p>
                     Your bookkeeper organize all of your transactions, including invoices and receipts, as well as prepare financial statements that you can use to assess the health of your operations. Will help you understand exactly where any issues lie, so that you can make necessary changes and address any issues immediately.
@@ -40,7 +40,7 @@
         </div>
         <div class="col-12 col-md-4">
             <div class="works_card px-5 pb-2 p-md-0 px-md-1 px-lg-2 px-xl-3">
-                <img class="work-clipart" src="../assets/build/svgs/deliver.svg" alt="What we do clipart">
+                <img class="work-clipart" src="../assets/build/svgs/deliver.svg" alt="What we do clipart" data-aos="fade-left"> 
                 <label>We deliver your tax-ready financials</label>
                 <p>
                     We help you make sense of your company’s finances. Track business health with monthly financial reporting, providing insight into what drives business performance. At tax time, we send a package with everything you need to file.
@@ -49,4 +49,13 @@
         </div>
     </div>
 </div>
-<div></div>
+<div class="section__b">
+    <div class="section__b__overlay z-1"></div>
+    <div class="container">
+        <div class="row h-100">
+            <div class="col d-flex justify-content-center align-items-center">
+                <span class="section__b__banner text-center text-white z-5 px-1">Smart Business Solutions That Make A Difference.</span>
+            </div>
+        </div>
+    </div>
+</div>
